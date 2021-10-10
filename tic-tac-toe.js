602f29662da566c12874e9f4108c20e9e280fd4c
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2021-10-10 13:47:09 -0500

    Updated script.js exercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,8 +6,11 @@
 */
 
 const all_spaces = [];
-const player1 = [];
-const player2 = [];
+var m = 0;
+const player_x = ["playerX", []];
+const player_o = ["playerO", []];
+var last_move = "";
+let count_moves = 9;
 const winning_spaces = [[0,1,2], [3,4,5], 
                         [6,7,8], [0,3,6], 
                         [1,4,7], [2,5,8],
@@ -19,20 +22,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (var i = 0; i < grids.length; i++){
     grids[i].classList.add("square");
-    clicked(grids, i);
   }
+
+  for (var i = 0; i < grids.length; i++){
+    clicked(player_o[1], player_x[1], grids, i);
+    hover(grids, i);
+  }
+
+
 });
 
-function clicked(positions, n) {
+//CLICK BOXES -> Only can click once
+function clicked(lst1, lst2, positions, n) {
   positions[n].addEventListener("click", function(){
-    if (positions[n].innerHTML != "X"){
+    if (last_move == "" || last_move == "O") {     
+      last_move = "X";
       positions[n].innerHTML = "X";
       positions[n].classList.add("X");
+      lst1.push(n);
+      console.log("x", lst1); 
     }
     else {     
+      last_move = "O"; 
       positions[n].innerHTML = "O";
       positions[n].classList.add("O"); 
+      lst2.push(n);    
+      console.log("o", lst2);   
     }
-  });
+  }, {once: true});  
+}
 
+//HOVER OVER BOX
+function hover(positions, n){
+  positions[n].addEventListener("mouseenter", function(){
+      positions[n].classList.add("hover"); 
+  }); 
+  
+  positions[n].addEventListener("mouseleave", function(){
+    positions[n].classList.remove("hover"); 
+}); 
 }
